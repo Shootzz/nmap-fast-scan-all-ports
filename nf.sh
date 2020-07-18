fd6f033f19abe0a629d5828b2a170c6fabca2507
Shootzz <cannburak9@gmail.com>
2020-07-18 15:35:34 +0300

    Update and rename nmapfast.sh to nf.sh

--- nf.sh
+++ nf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $# != 2 && $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-        nmap_ports=$(nmap -T4 -p- $1 --max-retries 2 | grep open | cut -d "/" -f 1 | tr -s "[:space:]" "," | sed 's/.$//');
+        nmap_ports=$(nmap -T4 -p- $1 --max-retries 4 | grep open | cut -d "/" -f 1 | tr -s "[:space:]" "," | sed 's/.$//');
         nmap -sV -sC -p ${nmap_ports} $1
 else
         echo "Error: Give a valid IP address as an argument"
